a169cbd8344088f2cce00ca37ca9dcaff7d03c5a
MarlonWSantos <marlon.santos.santos@icen.ufpa.br>
2020-03-19 21:58:45 -0300

    Inserido switch case na createCluster()

--- Cluster.java
+++ Cluster.java
@@ -1,12 +1,16 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import weka.clusterers.SimpleKMeans;
 import weka.core.Instances;
@@ -18,37 +22,19 @@ public class Cluster {
 
 	private static Instances data;
 	final static String PATH_CSV_FILE = "/tmp/motes_coordinates.csv";
+	final static String PATH_CSV_FILE_ACTIVES = "/tmp/actives_motes_coordinates.csv";
 	final static String PATH_ARFF_FILE = "/tmp/.motes_coordinates.arff";
+	static ArrayList<ArrayList<String>> dataFromFile;
+	static ArrayList<ArrayList<String>> motesActives;
+	protected static Graphic graphic = new Graphic();
+
+	static String[]  IPs = {"[aaaa::200:0:0:2]","[aaaa::200:0:0:3]","[aaaa::200:0:0:4]","[aaaa::200:0:0:5]","[aaaa::200:0:0:6]","[aaaa::200:0:0:7]","[aaaa::200:0:0:8]","[aaaa::200:0:0:9]","[aaaa::200:0:0:17]","[aaaa::200:0:0:25]","[aaaa::200:0:0:29]","[aaaa::200:0:0:1c]","[aaaa::200:0:0:a]","[aaaa::200:0:0:f]","[aaaa::200:0:0:1a]","[aaaa::200:0:0:17]","[aaaa::200:0:0:1f]"};
 
 
-	public BufferedReader readDataFile(String filename) {
-		BufferedReader inputReader = null;
-		try {
-			inputReader = new BufferedReader(new FileReader(filename));
 
-		} catch (FileNotFoundException ex) {
-			System.err.println("File not found: " + filename);
-		}
-		return inputReader;
-	}
+	public void convertCSV2Array() throws FileNotFoundException {
 
-	public void loadCSV() throws IOException {
-		CSVLoader loader = new CSVLoader();
-		loader.setSource(new File(PATH_CSV_FILE));
-		data = loader.getDataSet();
-	}
-
-	public void saveARFF() throws IOException {
-		ArffSaver saver = new ArffSaver();
-		saver.setInstances(data);
-		saver.setFile(new File(PATH_ARFF_FILE));
-		saver.setDestination(new File(PATH_ARFF_FILE));
-		saver.writeBatch();
-	}
-
-	public ArrayList<ArrayList<String>> convertCSV2Array() throws FileNotFoundException {
-
-		ArrayList<ArrayList<String>> dataFromFile=new ArrayList<ArrayList<String>>();
+		dataFromFile=new ArrayList<ArrayList<String>>();
 
 		try{
 			Scanner scanner=new Scanner(new FileReader(PATH_CSV_FILE));
@@ -65,39 +51,158 @@ public class Cluster {
 		}catch (FileNotFoundException e){
 			e.printStackTrace();
 		}
-		return dataFromFile;
+
 	}
 
-	public void readCSVInsertSeries(Graphic grap) throws FileNotFoundException  {
+	public void readEndAddressFindData( ) throws FileNotFoundException  {
+		int posicao=0;
+		motesActives=new ArrayList<ArrayList<String>>();
 
-		ArrayList<ArrayList<String>> list = new ArrayList<ArrayList<String>>();
+		String pattern = ":[1-9a-f]*]$";
 
-		list = convertCSV2Array();
+		Pattern EndIP = Pattern.compile(pattern);
 
-		for (int i=1;i<list.size();i++) {
-			grap.setCoordinatesSeries0(Double.parseDouble(list.get(i).get(1)),Double.parseDouble(list.get(i).get(2)));
+		for(int i=0;i<IPs.length;i++) {
+			Matcher matcher = EndIP.matcher(IPs[i]);
+			if (matcher.find( )) {
+				String end = matcher.group(0).replace(":","").replace("]","");
+				posicao = Integer.parseInt(end,16);
+				motesActives.add(dataFromFile.get(posicao-1));
+			}
 		}
 	}
-	
+
+	public void savingActivesMotesInCSV() throws IOException {
+		BufferedWriter br = new BufferedWriter(new FileWriter(PATH_CSV_FILE_ACTIVES));
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(dataFromFile.get(0).toString().replace("[","").replace("]",""));
+		sb.append("\n");
+
+		for (ArrayList<String> element : motesActives) {
+			sb.append(element.toString().replace("[","").replace("]",""));
+			sb.append("\n");
+		}
+
+		br.write(sb.toString());
+		br.close();
+	}
+	/********************************************************************************/	
+	public void loadCSV() throws IOException {
+		CSVLoader loader = new CSVLoader();
+		loader.setSource(new File(PATH_CSV_FILE_ACTIVES));
+		data = loader.getDataSet();
+	}
+
+	public void saveARFF() throws IOException {
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances(data);
+		saver.setFile(new File(PATH_ARFF_FILE));
+		saver.setDestination(new File(PATH_ARFF_FILE));
+		saver.writeBatch();
+	}
+	/********************************************************************************/	
 	public void createClusters() throws Exception {
-		
+
 		SimpleKMeans kmeans = new SimpleKMeans();
+
+		//Graphic graphic = new Graphic();
 		
+		graphic.createSerieCluster1();
+		graphic.createSerieCluster2();
+		graphic.createSerieCluster3();
+		graphic.createSerieCluster4();
+		graphic.createSerieCluster5();
+		graphic.createSerieCluster6();
+
+
 		kmeans.setSeed(10);
 		kmeans.setPreserveInstancesOrder(true);
-		kmeans.setNumClusters(3);
+		kmeans.setNumClusters(6);
 
 		BufferedReader datafile = readDataFile(PATH_ARFF_FILE);
 		Instances data = new Instances(datafile);
-		
-		kmeans.buildClusterer(data);
-		
-		int[] assignments = kmeans.getAssignments();
 
+		kmeans.buildClusterer(data);
+
+		int[] assignments = kmeans.getAssignments();
+		int mote=0;
+		double coordX;
+		double coordY;
 		int i=0;
 		for(int clusterNum : assignments) {
 			System.out.printf("Instance %d -> Cluster %d \n", i, clusterNum);
+			System.out.println(motesActives.get(mote));
+			
+			coordX=Double.parseDouble(motesActives.get(mote).get(1));
+			coordY=Double.parseDouble(motesActives.get(mote).get(2));
+
+			System.out.println();
+
+			switch (clusterNum) {
+			case 0:
+				graphic.setCoordinatesSeries1(coordX, coordY);
+				break;
+			case 1:
+				graphic.setCoordinatesSeries2(coordX, coordY);
+				break;
+			case 2:
+				graphic.setCoordinatesSeries3(coordX, coordY);
+				break;
+			case 3:
+				graphic.setCoordinatesSeries4(coordX, coordY);
+				break;
+			case 4:
+				graphic.setCoordinatesSeries5(coordX, coordY);
+				break;
+			case 5:
+				graphic.setCoordinatesSeries6(coordX, coordY);
+				break;			
+			}
+
 			i++;
+			mote++;
 		}
-	}	
+	}
+
+	public BufferedReader readDataFile(String filename) {
+		BufferedReader inputReader = null;
+		try {
+			inputReader = new BufferedReader(new FileReader(filename));
+
+		} catch (FileNotFoundException ex) {
+			System.err.println("File not found: " + filename);
+		}
+		return inputReader;
+	}
+
+	public static void main(String []args) throws FileNotFoundException {
+		Cluster obj = new Cluster();
+		
+		//Graphic grap = new Graphic();
+
+
+		try {
+			obj.convertCSV2Array();
+			obj.readEndAddressFindData();
+			obj.savingActivesMotesInCSV();
+			obj.loadCSV();
+			obj.saveARFF();
+			obj.createClusters();
+			
+			System.out.println(graphic.getCoordinateSeries1());
+			System.out.println(graphic.getCoordinateSeries2());
+			System.out.println(graphic.getCoordinateSeries3());
+			System.out.println(graphic.getCoordinateSeries4());
+			System.out.println(graphic.getCoordinateSeries5());
+			System.out.println(graphic.getCoordinateSeries6());
+
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
