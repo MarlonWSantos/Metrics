4914c09321c18b2a5e022e7215092aef281fc35b
MarlonWSantos <marlon.santos.santos@icen.ufpa.br>
2020-03-20 19:00:46 -0300

    Excluído as chamadas de função da classe Cluster

--- FirstTests.java
+++ FirstTests.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.File;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -20,7 +21,7 @@ import javafx.stage.Stage;
 public class FirstTests extends Application {
 	
 	private static ScatterChart<Number, Number> scNetwork;
-
+	protected static ScatterChart<Number,Number> sc;
 
 	public static void main(String[] args) throws IOException {
 		launch();
@@ -31,56 +32,30 @@ public class FirstTests extends Application {
 		stage.setTitle("Scatter Chart Sample");
 		final NumberAxis xAxis = new NumberAxis(0, 99, 1);
 		final NumberAxis yAxis = new NumberAxis(0, 99, 100);
-		ScatterChart<Number, Number> sc = new ScatterChart<Number, Number>(xAxis, yAxis);
+		sc = new ScatterChart<Number, Number>(xAxis, yAxis);
 		xAxis.setLabel("Age (years)");
 		yAxis.setLabel("Returns to date");
 		sc.setTitle("Investment Overview");
 		sc.setPrefSize(500, 400);
 		Scene scene = new Scene(new Group());
 		
-		Cluster obj = new Cluster();
-		
-		//Graphic grap = new Graphic();
-
-
-		try {
-			obj.convertCSV2Array();
-			obj.readEndAddressFindData();
-			obj.savingActivesMotesInCSV();
-			obj.loadCSV();
-			obj.saveARFF();
-			obj.createClusters();
+		new ThreadCluster(this);
 			
-			/*System.out.println(graphic.getCoordinateSeries1());
-			System.out.println(graphic.getCoordinateSeries2());
-			System.out.println(graphic.getCoordinateSeries3());
-			System.out.println(graphic.getCoordinateSeries4());
-			System.out.println(graphic.getCoordinateSeries5());
-			System.out.println(graphic.getCoordinateSeries6());
-*/
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		
-		sc.getData().add(Cluster.graphic.getCoordinateSeries1());
-		sc.getData().add(Cluster.graphic.getCoordinateSeries2());
-		sc.getData().add(Cluster.graphic.getCoordinateSeries3());
-		sc.getData().add(Cluster.graphic.getCoordinateSeries4());
-		sc.getData().add(Cluster.graphic.getCoordinateSeries5());
-		sc.getData().add(Cluster.graphic.getCoordinateSeries6());
-
-
 		((Group) scene.getRoot()).getChildren().add(sc);
 		stage.setScene(scene);
 		stage.show();
 	}
 	
-
-
-	
-
+	public void LoadGraphic() {
+		
+        if (!sc.getData().isEmpty()) {
+            sc.getData().remove(0,sc.getData().size());
+        }
+		sc.getData().add(Cluster.graphic.getCoordinateSeries1());
+		sc.getData().add(Cluster.graphic.getCoordinateSeries2());
+		sc.getData().add(Cluster.graphic.getCoordinateSeries3());
+		sc.getData().add(Cluster.graphic.getCoordinateSeries4());
+		sc.getData().add(Cluster.graphic.getCoordinateSeries5());
+		sc.getData().add(Cluster.graphic.getCoordinateSeries6());				
+	}
 }
